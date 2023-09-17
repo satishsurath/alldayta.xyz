73b77d581df4c5caa08e435aaa1411d452896f5d
Satish Surath <connect@sati.sh>
2023-09-17 10:00:22 -0400

    Removing caching for the pages rendered
    Signed-off-by: Satish Surath <connect@sati.sh>

--- app/routes.py
+++ app/routes.py
@@ -81,6 +81,19 @@ app.jinja_env.globals.update(escape_id=escape_id)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+# Used to not have chache on pages
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 
 
 # -------------------- Basic Admin Authentication --------------------
